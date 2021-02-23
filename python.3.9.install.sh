a67417ed74d85d63571f09a7cc0e63d1c8a39afa
mini <57116477+kmuhin@users.noreply.github.com>
2021-02-23 14:42:22 +0300

    Task 22.1d is complete

--- python.3.9.install.sh
+++ python.3.9.install.sh
@@ -2,7 +2,7 @@ sudo apt-get install build-essential checkinstall python3-dev python3-setuptools
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev
 sudo apt-get install libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev
 
-suffix='3.8.7'
+suffix='3.9.1'
 wget https://www.python.org/ftp/python/${suffix}/Python-${suffix}.tar.xz
 tar xvf Python-${suffix}.tar.xz
 cd Python-${suffix} || exit 1
